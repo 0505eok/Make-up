8319ef4c8f16a89e9fb3c67cbaeadb2ded259f5f
minseok <kms9887@naver.com>
2022-03-17 13:57:38 +0900

    test

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -3,7 +3,7 @@
 DEPLOY_PATH=/home/ec2-user/action/
 echo "> build 시작" >> /home/ec2-user/action/deploy.log
 echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/action/deploy.log
-cd DEPLOY_PATH
+cd $DEPLOY_PATH
 echo "현재경로 : $(pwd)" >> /home/ec2-user/action/deploy.log
 ./gradlew build
 echo "> build 완료" >> /home/ec2-user/action/deploy.log
